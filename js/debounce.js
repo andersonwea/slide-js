64b7fd55c7adf4e663608142ffcef31b30d8d145
chain <klaus.mika1052@gmail.com>
2023-04-01 13:13:44 -0300

    primeiro commit

---
+++ js/debounce.js
@@ -0,0 +1,13 @@
+const debounce = (callback, delay) => {
+  let timer;
+
+  return (...args) => {
+    if (timer) clearInterval(timer);
+    timer = setTimeout(() => {
+      callback(...args);
+      timer = null;
+    }, delay);
+  };
+};
+
+export default debounce;
