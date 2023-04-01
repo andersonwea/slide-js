6b85843c188de2818530dc08fe755baad0933a25
chain <klaus.mika1052@gmail.com>
2023-04-01 13:15:12 -0300

    adiconados os eventos de click de mouseover e mousemove ao slide

---
+++ js/slide.js
@@ -0,0 +1,40 @@
+class Slide {
+  constructor(wrapper, slide) {
+    this.wrapper = document.querySelector(wrapper);
+    this.slide = document.querySelector(slide);
+  }
+
+  onStart(event) {
+    event.preventDefault();
+    console.log("down");
+    this.wrapper.addEventListener("mousemove", this.onMove);
+  }
+
+  onMove(event) {
+    console.log("move");
+  }
+
+  onEnd(event) {
+    console.log("up");
+    this.wrapper.removeEventListener("mousemove", this.onMove);
+  }
+
+  addSlideEvents() {
+    this.wrapper.addEventListener("mousedown", this.onStart);
+    this.wrapper.addEventListener("mouseup", this.onEnd);
+  }
+
+  bindEvents() {
+    this.onStart = this.onStart.bind(this);
+    this.onMove = this.onMove.bind(this);
+    this.onEnd = this.onEnd.bind(this);
+  }
+
+  init() {
+    this.bindEvents();
+    this.addSlideEvents();
+    return this;
+  }
+}
+
+export default Slide;
